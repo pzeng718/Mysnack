5c1f3541c634bad559c40e0e835531500af11d47
Peter Zeng <pzeng718@gmail.com>
2021-04-12 20:19:40 +0800

    added more products and styling

--- js/index.js
+++ js/index.js
@@ -28,17 +28,59 @@ let products = [
         'price': 2.99,
         'qty': 15,
         'image': '../resource/popcorn.jpg'
+    },
+    {
+        'name': 'milk',
+        'price': 4.99,
+        'qty': 15,
+        'image': '../resource/milk.jpg'
+    },
+    {
+        'name': 'dove',
+        'price': 5.99,
+        'qty': 10,
+        'image': '../resource/dove.jpg'
+    },
+    {
+        'name': 'cheez-it',
+        'price': 4.99,
+        'qty': 10,
+        'image': '../resource/cheez-it.png'
+    },
+    {
+        'name': 'cheetos',
+        'price': 1.99,
+        'qty': 20,
+        'image': '../resource/cheetos.jpg'
+    },
+    {
+        'name': 'fritos',
+        'price': 1.99,
+        'qty': 10,
+        'image': '../resource/fritos.jpg'
+    },
+    {
+        'name': 'doritos-L',
+        'price': 4.99,
+        'qty': 15,
+        'image': '../resource/doritos-L.png'
     }
 ]
 
 let productBodyElement = jQuery(".Product");
 
+let productHtml = '<div class="product-container">'
 for (let product of products) {
-    let productHtml = '<li>';
-    productHtml += `Name: ${product.name}<ul>`;
-    productHtml += `<li>Price: ${product.price}</li>`
-    productHtml += `<li>Quantity: ${product.qty}</li>`
-    productHtml += '</li>';
+    productHtml += '<div class="product-item">';
+    productHtml += '<div class="product-title">'
+
+    productHtml += `<p>Name: ${product.name}</p>`;
+    productHtml += `<p>Price: ${product.price}</p>`
+    productHtml += `<p>Quantity: ${product.qty}</p>`
+    productHtml += '</div>'
+
     productHtml += `<img src="${product.image}" alt=${product.name} style="width:400px;height:300px;"></img>`
-    productBodyElement.append(productHtml);
+    productHtml += '</div>';
 }
+productHtml += '</div>'
+productBodyElement.append(productHtml);
